f3fece28b4300c65dd62bee80a0909c24db797b4
Fengtan <Fengtan@847318.no-reply.drupal.org>
2014-12-16 23:17:27 -0500

    Add cross.

--- burndown.py
+++ burndown.py
@@ -23,11 +23,22 @@ plughelp.add('burndown', 'show a burndown chart of the current iteration') # TOD
 
 def handle_burndown(bot, ievent):
     """ burndown .. show burndown chart of the current iteration """ #TODO drop
-    lines = 12 # number of lines used to print the chart -- TODO define in constant or set as an option ?
+    lines   = 12 # number of lines used to print the chart -- TODO define in constant or set as an option ?
+    max_day = 15 # TODO dynamic
+    day     = 10
+    max_sp  = 60
+    sp      = 20
     for line in range(0, lines):
         # TODO spread slope according to height
-        padding = ' ' * line
-        ievent.reply("|%s\\" % padding)
+        padding_1 = ' ' * line                     # padding before '\'
+        padding_2 = ' ' * (lines - len(padding_1)) # padding after  '\'
+        string = "|%s\\%s" % (padding_1, padding_2)
+        # Set cross 
+        if int(sp*lines/max_sp) == line:
+            string = list(string)
+            string[int(day*lines/max_day)] = '+'
+            string = "".join(string)
+        ievent.reply(string)
     ievent.reply('|' + '_' * lines)
 
 # TODO time between each reply -- find a way to reduce that
