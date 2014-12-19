7679a654b3ad8fa71eda07ae0c7048abbc42ed16
Fengtan <Fengtan@847318.no-reply.drupal.org>
2014-12-18 20:27:56 -0500

    Refactor.

--- burndown.py
+++ burndown.py
@@ -1,21 +1,46 @@
 # plugs/burndown.py
 #
 # TODO readme
+# Update functions to make the chart dynamic, e.g. by querying your ticketing system.
+# TODO support floats (e.g. 10.5 SP)
 
-__copyright__ = 'GPLv3 https://www.gnu.org/licenses/gpl-3.0.html' # TODO
+__copyright__ = 'GPLv3 https://www.gnu.org/licenses/gpl-3.0.html'
 
 from gozerbot.commands import cmnds
 from gozerbot.examples import examples
 from gozerbot.plughelp import plughelp
 
+# Return number of days in the iteration.
+def get_day_maximum():
+    return 20
+
+# Return current day in the iteration [between 0 and get_day_maximum()].
+def get_day_current():
+    return 10
+
+# Return total number of story points in the iteration.
+def get_spt_maximum():
+    return 80
+
+# Return number of story points left in the iteration [between 0 and get_spt_maximum()].
+def get_spt_current():
+    return 30
+
+# Replace character located at 'position' in 'string' with 'replacement'.
+def replace_char(string, replacement, position):
+    string = list(string)
+    string[position] = '+'
+    string = "".join(string)
+    return string
+
 # Define callback.
 def handle_burndown(bot, ievent):
     """ burndown .. show burndown chart of the current iteration """
-    lines   = 12 # number of lines used to print the chart -- TODO define in constant or set as an option ?
-    day_maximum = 15 # TODO dynamic + doc
-    day_current = 10
-    spt_maximum = 60
-    spt_current = 20
+    lines       = 12 # Number of lines used to print the chart.
+    day_maximum = get_day_maximum()
+    day_current = get_day_current()
+    spt_maximum = get_spt_maximum()
+    spt_current = get_spt_current()
     for line in range(0, lines):
         # Build padding before (1) and after (2) the curve.
         padding_1 = ' ' * line
@@ -26,14 +51,12 @@ def handle_burndown(bot, ievent):
         elif line == 1:
             spt = 'SP'
         else:
-            spt = '  ' # TODO what if 100 max
+            spt = '  '
         # Format line.
         string = "%s|%s\\%s" % (spt, padding_1, padding_2)
         # Set cross on appropriate line.
         if int(spt_current*lines/spt_maximum) == line:
-            string = list(string)
-            string[int(day_current*lines/day_maximum)] = '+'
-            string = "".join(string)
+            string = replace_char(string, '+', int(day_current*lines/day_maximum))
         # Display line.
         ievent.reply(string)
     # Display chart footer.
